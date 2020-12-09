17995c68ff2e2767872c535dd2c0802c6f937511
finefrezy <bru_ce_tao@126.com>
2020-12-09 11:24:12 +0800

    消息队列

---
+++ RabbitMQDemo/src/main/java/com/cx/demo02/Work03.java
@@ -0,0 +1,47 @@
+package com.cx.demo02;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Work03 {
+    private final static String QUEUE_NAME = "hello-demo1";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // 一次只接受一条未确认的消息
+        channel.basicQos(1);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+
+            try {
+                doWork(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(" [x] Done");
+                // 新增确认     channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+
+        boolean autoAck = false;// 修改
+        channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
+    }
+}
---
+++ RabbitMQDemo/src/main/java/com/cx/demo02/demo/NewTask01.java
@@ -0,0 +1,32 @@
+package com.cx.demo02.demo;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask01 {
+
+    // 定义队列名称
+    private final static String QUEUE_NAME = "queue-task";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel()) {
+            // 声明为true
+            boolean durable = true;
+            channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
+
+            String message = "Fifth message......................................";
+
+            channel.basicPublish("", "",
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+
+}
